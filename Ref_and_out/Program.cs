e223247a193acf27e555688375efdda5edbf1cfd
John Cokins <il7148pg@go.minnstate.edu>
2020-02-13 03:28:44 -0600

    Add project files.

---
+++ Ref_and_out/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Ref_and_out
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
